bc8227bc85a5488e655b87d8b6d00f047190bec3
zztkm <zztkm@tsurutatakumi.info>
2024-04-06 08:50:09 +0900

    init & 第一章

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zztkm/go-wakaru-system
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// VS Code と dlv を使って、デバッグを行い、
+	// fmt.Println の中で何が起こっているかを確認した
+	// めっちゃ楽しい
+	fmt.Println("Hello, World!")
+}
