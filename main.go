30cb55feaabc44834139894f4db54e00b847273f
zztkm <zztkm@tsurutatakumi.info>
2024-04-06 19:49:25 +0900

    第2章 Q2.1

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	// VS Code と dlv を使って、デバッグを行い、
 	// fmt.Println の中で何が起こっているかを確認した
 	// めっちゃ楽しい
-	fmt.Println("Hello, World!")
+	f, err := os.Create("hello.txt")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprintf(f, "私は %d 歳です", 25)
 }
